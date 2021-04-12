d91c34564a48b9202f1635b006aac9ba4ac5513a
Dev Khalil <devkhalil1337@gmail.com>
2021-04-12 06:48:50 +0500

    crypto list updated from 50 to 250 and dynamic scrollbar added

--- www/js/Controllers/cryptoPricessController.js
+++ www/js/Controllers/cryptoPricessController.js
@@ -1,12 +1,15 @@
 angular.module('myApp').controller("cryptoPricessController", function ($scope, $q, $interval, $rootScope, navigationService,apiService,localStorageService) {
 
     $scope.MarketPrices = {};
+    $scope.totalDisplayed = 20;
 
     function init() {
         console.log("I am crypto");
         $scope.getAllCoinsPrices();
     }
 
+
+    $scope.loadMore = () =>  $scope.totalDisplayed += 20;
 
     $scope.getAllCoinsPrices = async function(){
         let curr = localStorageService.getCurrency();
