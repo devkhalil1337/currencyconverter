5097202f092b49d3af9357e87f384d8e9e8ff059
devkhalil1337 <devkhalil1337@gmail.com>
2024-06-22 15:17:55 +0500

    platform updated

---
+++ platforms/android/app/src/main/assets/www/js/Services/apiService.js
@@ -0,0 +1,49 @@
+angular.module('myApp').factory('apiService', function ($http, $q) {
+    var apiBaseUrl = "https://api.coingecko.com/api/v3/";
+    var factory = {};
+    factory.getAllSupportedCurrencies = function () {
+        let url = apiBaseUrl + "simple/supported_vs_currencies";
+        var req = {
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-Type': "application/json"
+            },
+        }
+        return $http(req);
+    }
+    factory.getAllSupportedCurrenciesFullName = function () {
+        let url = apiBaseUrl + "coins/list";
+        var req = {
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-Type': "application/json"
+            },
+        }
+        return $http(req);
+    }
+    factory.getCurrenciesPrice = function (data) {
+        let url = apiBaseUrl + "simple/price?ids="+data.selectedFromCurr+"&vs_currencies="+data.selectedToCurr;
+        var req = {
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-Type': "application/json"
+            },
+        }
+        return $http(req);
+    }
+    factory.getMarketPrices = function (data) {
+        let url = apiBaseUrl + "coins/markets?vs_currency="+data.name+"&order=market_cap_desc&per_page=250&page=1&sparkline=false";
+        var req = {
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-Type': "application/json"
+            },
+        }
+        return $http(req);
+    }
+    return factory;
+});
---
+++ platforms/android/app/src/main/assets/www/js/allcurrencies.js
@@ -0,0 +1,56 @@
+var allcurrencies = [
+    {
+        id:1,
+        coinId:"bitcoin",
+        name:"Bitcoin",
+        symbol:"BTC"
+    },
+    {
+        id:2,
+        coinId:"ethereum",
+        name:"Ethereum",
+        symbol:"ETH"
+    },
+    {
+        id:3,
+        coinId:"binancecoin",
+        name:"Binance Coin",
+        symbol:"bnb"
+    },
+    {
+        id:4,
+        coinId:"bitcoin-cash",
+        name:"Bitcoin Cash",
+        symbol:"bch"
+    },
+    {
+        id:5,
+        coinId:"bitcoin-cash",
+        name:"Bitcoin Cash",
+        symbol:"bch"
+    },
+    {
+        id:6,
+        coinId:"pkr",
+        name:"Pakistani Ruppees",
+        symbol:"PKR"
+    },
+    {
+        id:7,
+        coinId:"inr",
+        name:"Indian Ruppees",
+        symbol:"INR"
+    },
+    {
+        id:8,
+        coinId:"usd",
+        name:"USD",
+        symbol:"usd"
+    },
+    {
+        id:9,
+        coinId:"eur",
+        name:"Euro",
+        symbol:"eur"
+    },
+]
